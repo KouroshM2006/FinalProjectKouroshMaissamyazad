e057d709238f57859eaf1ed718dd21781d7f6a98
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-04-30 17:59:06 -0400

    full implementation of AcademicItem with possibility of change in the future

--- src/main/java/org/example/AcademicItem.java
+++ src/main/java/org/example/AcademicItem.java
@@ -1,4 +1,79 @@
 package org.example;
 
-public abstract class AcademicItem {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Objects;
+
+public abstract class AcademicItem implements Comparable<AcademicItem> {
+    protected String title;
+    protected String description;
+    protected LocalDate createdDate;
+
+    public AcademicItem(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.createdDate = LocalDate.now();
+    }
+
+    /**
+     * returns the type of the AcademicItem (Course, Assignment) as a string
+     * @return string
+     */
+    public abstract String getType();
+
+    /**
+     * sorts AcademicItems according to createdDate
+     * @param o the object to be compared.
+     * @return the order of the objects
+     */
+    @Override
+    public int compareTo(AcademicItem o) {
+        return this.createdDate.compareTo(o.createdDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AcademicItem that = (AcademicItem) o;
+        return Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(createdDate, that.createdDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, createdDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AcademicItem{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", createdDate=" + createdDate +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
 }
