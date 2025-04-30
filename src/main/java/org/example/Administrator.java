7567e1494d29504a336b6cb5f856d17c0f7259fa
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-04-29 20:36:29 -0400

    implementation of Administrator class and CreateUser interface with possibility of change in the future

--- src/main/java/org/example/Administrator.java
+++ src/main/java/org/example/Administrator.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class Administrator extends User{
+public class Administrator extends User implements CreateUser{
+
+    public Administrator(String name, int age, Gender gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public User createTeacher(String name, int age, Gender gender) {
+        return new Teacher(name, age, gender);
+    }
+
+    @Override
+    public User createStudent(String name, int age, Gender gender) {
+        return new Student(name, age, gender);
+    }
+
+    @Override
+    public String getRole() {
+        return "Administrator";
+    }
 }
--- src/main/java/org/example/CreateUser.java
+++ src/main/java/org/example/CreateUser.java
@@ -1,4 +1,6 @@
 package org.example;
 
 public interface CreateUser {
+    User createTeacher(String name, int age, User.Gender gender);
+    User createStudent(String name, int age, User.Gender gender);
 }
