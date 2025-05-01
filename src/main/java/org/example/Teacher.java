05962f9c19682e09bc8b21bbd30c5cb24eed3290
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-04-30 20:37:25 -0400

    semi implementation of the Teacher class

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,4 +1,20 @@
 package org.example;
+import java.util.*;
 
-public class Teacher {
+public class Teacher extends User{
+    private List<Course> courses;
+
+    public Teacher(String name, int age, Gender gender) {
+        super(name, age, gender);
+        this.courses = new ArrayList<>();
+    }
+
+    /**
+     * Teacher's version of getRole
+     * @return "Teacher"
+     */
+    @Override
+    public String getRole() {
+        return "Teacher";
+    }
 }
