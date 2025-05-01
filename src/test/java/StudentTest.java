6d3b71fafe77254ef7cb3de94f762c24b2031aba
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-04-30 20:43:16 -0400

    Added Junit testing for the Student class

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,39 @@
+import org.example.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StudentTest {
+
+    @Test
+    public void testDownloadSchedule() {
+        Student student = new Student("bob", 22, User.Gender.MALE);
+        Map<LocalDate, LocalTime> map = new TreeMap<>();
+        map.put(LocalDate.now(), LocalTime.now());
+        Teacher teacher = new Teacher("john", 56, User.Gender.MALE);
+        student.getSchedule().add(new Course("english","xxxx",teacher,new ArrayList<>(),map));
+
+        student.downloadSchedule();
+
+        File file = new File("src/main/java/Resources/bob_schedule.csv");
+        Assertions.assertTrue(file.exists());
+
+        try {
+            String content = Files.readString(file.toPath());
+            Assertions.assertTrue(content.contains("john"));
+            Assertions.assertTrue(content.contains("english"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        file.delete();
+    }
+}
