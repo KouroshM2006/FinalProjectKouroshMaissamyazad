5045721352f276cdfce19780b9e389f37b4e546b
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-05-09 19:59:40 -0400

    implemented new methods from creatable interface and brought minor changes

--- src/main/java/org/example/Administrator.java
+++ src/main/java/org/example/Administrator.java
@@ -1,23 +1,148 @@
 package org.example;
 
-public class Administrator extends User implements CreateUser{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Administrator extends User implements Creatable {
+    private long adminId;
+
+    private static long nextId = 1;
 
     public Administrator(String name, int age, Gender gender) {
         super(name, age, gender);
+        this.adminId = nextId++;
     }
 
+
+    /**
+     * creates a new Teacher class and adds it to the user list in CourseManagementSystem
+     *
+     * @param name
+     * @param age
+     * @param gender
+     */
     @Override
-    public User createTeacher(String name, int age, Gender gender) {
-        return new Teacher(name, age, gender);
+    public void createTeacher(String name, int age, Gender gender) {
+        CourseManagementSystem.users.add(new Teacher(name, age, gender));
+        System.out.println("Teacher successfully created");
     }
 
+    /**
+     * creates a new Student class and adds it to the user list in CourseManagementSystem
+     *
+     * @param name
+     * @param age
+     * @param gender
+     */
     @Override
-    public User createStudent(String name, int age, Gender gender) {
-        return new Student(name, age, gender);
+    public void createStudent(String name, int age, Gender gender) {
+        CourseManagementSystem.users.add(new Student(name, age, gender));
+        System.out.println("Student successfully created");
+
     }
 
+    /**
+     * Administrator's version of getRole
+     *
+     * @return "Administrator"
+     */
     @Override
     public String getRole() {
         return "Administrator";
+    }
+
+    /**
+     * method to import users from users.csv file in the Resources folder
+     */
+    public void importUsers() {
+        File file = new File("src/main/java/Resources/users.csv");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String[] data = scanner.nextLine().split(",");
+                String name = data[1];
+                int age = Integer.parseInt(data[2]);
+                User.Gender gender = User.Gender.valueOf(data[3]);
+
+                switch (data[0].toLowerCase()) {
+                    case "administrator":
+                        CourseManagementSystem.users.add(new Administrator(name, age, gender));
+                        break;
+                    case "teacher":
+                        CourseManagementSystem.users.add(new Teacher(name, age, gender));
+                        break;
+                    case "student":
+                        CourseManagementSystem.users.add(new Student(name, age, gender));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Administrator's implementation of createCourse method
+     *
+     * @param title       given title as String
+     * @param description given description as String
+     * @param teacher     given Teacher
+     * @param dateAndTime given map of dates and times
+     * @param capacity    given student capacity of the course
+     */
+    @Override
+    public void createCourse(String title, String description, Teacher teacher, Map<LocalDate, LocalTime> dateAndTime, int capacity) {
+        if (teacher.getCourses().stream().filter(course -> course.title.equalsIgnoreCase(title)).toList().isEmpty()) {
+            Course course = new Course(title, description, teacher, dateAndTime, capacity);
+            teacher.getCourses().add(course);
+            CourseManagementSystem.courses.add(course);
+            System.out.println("course successfully created");
+        } else {
+            System.out.println("Course creation failed - The teacher already holds a course with the same title");
+        }
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Administrator that = (Administrator) o;
+        return adminId == that.adminId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), adminId);
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "adminId=" + adminId +
+                '}' + super.toString();
+    }
+
+    public long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(long adminId) {
+        this.adminId = adminId;
+    }
+
+    public static long getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(long nextId) {
+        Administrator.nextId = nextId;
     }
 }
--- src/test/java/AdministratorTest.java
+++ src/test/java/AdministratorTest.java
@@ -1,38 +1,74 @@
-import org.example.Administrator;
-import org.example.Student;
-import org.example.Teacher;
-import org.example.User;
+import org.example.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+import java.util.TreeMap;
 
 public class AdministratorTest {
     @Test
     public void createTeacherTest() {
         Administrator admin = new Administrator("xoxo", 56, User.Gender.MALE);
+        admin.createTeacher("Bob", 23, User.Gender.MALE);
 
-        User expected = new Teacher("bob", 45, User.Gender.MALE);
-        User result = admin.createTeacher("bob", 45, User.Gender.MALE);
+        Teacher teacher = new Teacher("Bob", 23, User.Gender.MALE);
+        teacher.setTeacherId(1);
 
-        Assertions.assertEquals(result.getName(), expected.getName());
-        Assertions.assertEquals(result.getAge(), expected.getAge());
-        Assertions.assertEquals(result.getGender(), expected.getGender());
-        Assertions.assertEquals(result.getRole(), expected.getRole());
+        Assertions.assertTrue(CourseManagementSystem.users.contains(teacher));
     }
 
     @Test
-    public void createTest() {
+    public void createStudentTest() {
         Administrator admin = new Administrator("xoxo", 56, User.Gender.MALE);
+        admin.createStudent("bob", 45, User.Gender.MALE);
 
-        User expected = new Student("bob", 45, User.Gender.MALE);
-        User result = admin.createStudent("bob", 45, User.Gender.MALE);
+        Student student = new Student("bob", 45, User.Gender.MALE);
+        student.setStudentId(1);
 
-        Assertions.assertEquals(result.getName(), expected.getName());
-        Assertions.assertEquals(result.getAge(), expected.getAge());
-        Assertions.assertEquals(result.getGender(), expected.getGender());
-        Assertions.assertEquals(result.getRole(), expected.getRole());
+        Assertions.assertTrue(CourseManagementSystem.users.contains(student));
     }
 
+    @Test
+    public void importUserTest() {
+        Administrator admin = new Administrator("bob", 30, User.Gender.MALE);
+        admin.importUsers();
+
+        Teacher teacher = new Teacher("Marvin", 23, User.Gender.MALE);
+        teacher.setTeacherId(1);
+        Administrator administrator = new Administrator("Josh", 56, User.Gender.MALE);
+        administrator.setAdminId(2);
+        Student student = new Student("Jack", 12, User.Gender.MALE);
+        student.setStudentId(1);
+
+        Assertions.assertTrue(CourseManagementSystem.users.contains(teacher));
+        Assertions.assertTrue(CourseManagementSystem.users.contains(administrator));
+        Assertions.assertTrue(CourseManagementSystem.users.contains(student));
+    }
+
+    @Test
+    public void createCourseTest1() {
+        Administrator admin = new Administrator("bob", 30, User.Gender.MALE);
+        Teacher teacher = new Teacher("josh", 34, User.Gender.MALE);
+        admin.createCourse("english", "english", teacher, new TreeMap<>(), 30);
+
+        Course expected = new Course("english", "english", teacher, new TreeMap<>(), 30);
+        expected.setCourseId(1);
+
+        Assertions.assertTrue(CourseManagementSystem.courses.contains(expected));
+        Assertions.assertTrue(teacher.getCourses().contains(expected));
+    }
+
+    @Test
+    public void createCourseTest2() {
+        Administrator admin = new Administrator("bob", 30, User.Gender.MALE);
+        Teacher teacher = new Teacher("josh", 34, User.Gender.MALE);
+        admin.createCourse("english", "english", teacher, new TreeMap<>(), 30);
+
+        admin.createCourse("english", "english", teacher, new TreeMap<>(), 30);
+
+        Course expected = new Course("english", "english", teacher, new TreeMap<>(), 30);
+
+        Assertions.assertFalse(CourseManagementSystem.courses.contains(expected));
+        Assertions.assertFalse(teacher.getCourses().contains(expected));
+    }
 
 }
