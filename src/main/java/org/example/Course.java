d4a66e34f9abbadc08114f756d9d935b1e4b8c87
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-05-09 20:03:54 -0400

    Brought minor changes to Course class

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -3,35 +3,30 @@ package org.example;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Course extends AcademicItem {
     private long courseId;
     private Teacher teacher;
     private List<Student> students;
-    private Map<LocalDate, LocalTime> DateAndTime;
+    private Map<LocalDate, LocalTime> dateAndTime;
+    private int capacity;
 
     private static long nextId = 1;
 
     //constructor with auto increasing ID
-    public Course(String title, String description, Teacher teacher, List<Student> students, Map<LocalDate, LocalTime> dateAndTime) {
+    public Course(String title, String description, Teacher teacher, Map<LocalDate, LocalTime> dateAndTime, int capacity) {
         super(title, description);
         this.courseId = nextId++;
         this.teacher = teacher;
-        this.students = students;
-        DateAndTime = dateAndTime;
-    }
-
-    //all arguments constructor
-    public Course(String title, String description, long courseId, Teacher teacher, List<Student> students, Map<LocalDate, LocalTime> dateAndTime) {
-        super(title, description);
-        this.courseId = courseId;
-        this.teacher = teacher;
-        this.students = students;
-        DateAndTime = dateAndTime;
+        this.students = new ArrayList<>();
+        this.dateAndTime = dateAndTime;
+        this.capacity = capacity;
     }
 
     /**
      * Course's version of getType
+     *
      * @return "Course"
      */
     @Override
@@ -41,37 +36,43 @@ public class Course extends AcademicItem {
 
     /**
      * adds a student to the students list if the student is not already registered
+     *
      * @param student given student to add
      * @return boolean value on whether the student was added or not
      */
-    public boolean addStudent(Student student) {
-        if (students.contains(student)) return false;
-        students.add(student);
-        return true;
+    public void addStudent(Student student) {
+        if (this.students.size() < capacity) {
+            students.add(student);
+            student.getCourses().add(this);
+            System.out.println("Student successfully enrolled");
+        } else {
+            System.out.println("the maximum capacity of students has been reached for this course");
+        }
     }
-
-    // Todo: add CourseComparator class
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Course course = (Course) o;
-        return courseId == course.courseId && Objects.equals(teacher, course.teacher) && Objects.equals(students, course.students) && Objects.equals(DateAndTime, course.DateAndTime);
+        return courseId == course.courseId;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), courseId, teacher, students, DateAndTime);
+        return Objects.hash(super.hashCode(), courseId);
     }
 
     @Override
     public String toString() {
         return "Course{" +
                 "courseId=" + courseId +
-                ", teacher=" + teacher +
-                ", students=" + students +
-                ", DateAndTime=" + DateAndTime +
+                ", teacher=" + teacher.getName() +
+                ", students=" + students.stream()
+                .map(student -> "(Id: " + student.getStudentId() + ", name: " + student.getName() + ")")
+                .collect(Collectors.joining(", ")) +
+                ", dateAndTime=" + dateAndTime +
+                ", capacity=" + capacity +
                 '}' + super.toString();
     }
 
@@ -100,11 +101,19 @@ public class Course extends AcademicItem {
     }
 
     public Map<LocalDate, LocalTime> getDateAndTime() {
-        return DateAndTime;
+        return dateAndTime;
     }
 
     public void setDateAndTime(Map<LocalDate, LocalTime> dateAndTime) {
-        DateAndTime = dateAndTime;
+        this.dateAndTime = dateAndTime;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
     public static long getNextId() {
@@ -114,4 +123,36 @@ public class Course extends AcademicItem {
     public static void setNextId(long nextId) {
         Course.nextId = nextId;
     }
+
+    //CourseComparator subclass
+    public static class CourseComparator implements Comparator<Course> {
+        private String type;
+
+        public CourseComparator(String type) {
+            this.type = type;
+        }
+
+        /**
+         * compares Course classes based on a given parameter
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return whether the compared object should go first, last or order does not matter
+         */
+        @Override
+        public int compare(Course o1, Course o2) {
+            switch (type.toLowerCase()) {
+                case "title":
+                    return o1.title.compareTo(o2.title);
+                case "enrollment count":
+                    return o1.getStudents().size() - o2.getStudents().size();
+                case "capacity" :
+                    return o1.getCapacity() - o2.getCapacity();
+                default:
+                    return (int) o1.getCourseId() - (int) o2.getCourseId();
+            }
+        }
+
+    }
 }
+
--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,35 +1,71 @@
-import org.example.Course;
-import org.example.Student;
-import org.example.Teacher;
-import org.example.User;
+import org.example.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.TreeMap;
+import java.util.*;
 
 public class CourseTest {
 
     @Test
     public void addStudentTest1() {
         Teacher teacher = new Teacher("john", 56, User.Gender.MALE);
-        Course course = new Course("english", "xxxx", teacher, new ArrayList<>(), new TreeMap<>());
+        Course course = new Course("english", "xxxx", teacher, new TreeMap<>(), 0);
 
         Student input = new Student("john", 12, User.Gender.MALE);
+        course.addStudent(input);
 
-        Assertions.assertTrue(course.addStudent(input));
-        Assertions.assertTrue(course.getStudents().contains(input));
+        Assertions.assertFalse(course.getStudents().contains(input));
     }
 
     @Test
     public void addStudentTest2() {
         Teacher teacher = new Teacher("john", 56, User.Gender.MALE);
-        Course course = new Course("english", "xxxx", teacher, new ArrayList<>(), new TreeMap<>());
+        Course course = new Course("english", "xxxx", teacher, new TreeMap<>(), 7);
 
         Student input = new Student("john", 12, User.Gender.MALE);
         course.addStudent(input);
 
-        Assertions.assertFalse(course.addStudent(input));
         Assertions.assertTrue(course.getStudents().contains(input));
+    }
+
+    @Test
+    public void courseComparatorClassTest() {
+        Teacher teacher = new Teacher("john", 56, User.Gender.MALE);
+        Student student = new Student("bob", 12, User.Gender.MALE);
+
+        Course c1 = new Course("english", "xxx", teacher, new TreeMap<>(), 14);
+        Course c2 = new Course("french", "xxx", teacher, new TreeMap<>(), 30);
+        Course c3 = new Course("programming", "xxx", teacher, new TreeMap<>(), 20);
+
+        c1.addStudent(student);
+
+        c2.addStudent(student);
+        c2.addStudent(student);
+
+        c3.addStudent(student);
+        c3.addStudent(student);
+        c3.addStudent(student);
+
+        List<Course> courses = new ArrayList<>(List.of(c2, c3, c1));
+        List<Course> expected = new ArrayList<>(List.of(c1, c2, c3));
+
+        Collections.sort(courses, new Course.CourseComparator("title"));
+
+        Assertions.assertEquals(expected, courses);
+
+        Collections.sort(courses, new Course.CourseComparator("enrollment count"));
+
+        Assertions.assertEquals(expected, courses);
+
+        Collections.sort(courses, new Course.CourseComparator("capacity"));
+        expected = new ArrayList<>(List.of(c1, c3, c2));
+
+        Assertions.assertEquals(expected, courses);
+
+        Collections.sort(courses, new Course.CourseComparator(""));
+        expected= new ArrayList<>(List.of(c1, c2, c3));
+
+        Assertions.assertEquals(expected, courses);
+
     }
 }
