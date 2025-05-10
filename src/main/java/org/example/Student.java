c8f0b949735a29e922d7fa89250c9f75bb8c74e1
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-05-09 20:04:20 -0400

    100% implementation of Student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,20 +3,29 @@ package org.example;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Student extends User {
-    private List<Course> schedule;
+    private long studentId;
+    private List<Course> courses;
     private List<Assignment> assignments;
+    private final int capacity = 8;
+
+    private static int nextId = 1;
 
     public Student(String name, int age, Gender gender) {
         super(name, age, gender);
-        this.schedule = new ArrayList<>();
+        this.studentId = nextId++;
+        this.courses = new ArrayList<>();
         this.assignments = new ArrayList<>();
     }
 
+
     /**
      * Students version of getRole
+     *
      * @return "Student"
      */
     @Override
@@ -32,7 +41,7 @@ public class Student extends User {
 
         try (FileWriter fw = new FileWriter(file)) {
             fw.write("courseId,Title,Teacher,DateAndTime\n");
-            for (Course course : schedule) {
+            for (Course course : courses) {
                 fw.write(String.format("%s,%s,%s,%s\n", course.getCourseId(), course.getTitle(), course.getTeacher().getName(), course.getDateAndTime()));
             }
         } catch (IOException e) {
@@ -41,33 +50,98 @@ public class Student extends User {
 
     }
 
+    /**
+     * method to enroll the student into a course
+     * @param course given Course
+     */
+    public void enrollCourse(Course course) {
+        if (courses.size() >= capacity) {
+            System.out.println("maximum capacity of courses has been reached");
+        } else if (courses.contains(course)) {
+            System.out.println("Student is already registered to the course");
+        } else {
+            course.addStudent(this);
+        }
+    }
+
+    /**
+     * method to submit an assignment
+     *
+     * @param assignment     given assignment to be submitted
+     * @param submissionFile given submissionFile
+     */
+    public void submitAssignment(Assignment assignment, File submissionFile) {
+        if (!assignments.contains(assignment)) {
+            System.out.println("Assignment does not exist");
+        } else if (assignment.getDueDate().isAfter(LocalDateTime.now())) {
+            assignment.addSubmission(this, submissionFile);
+            System.out.println("Assignment submitted successfully");
+        } else {
+            System.out.println("Submission denied - past deadline!");
+        }
+    }
+
+    /**
+     * method to view the feedback left by the teacher on an assignment
+     *
+     * @param assignment given assignment to view
+     */
+    public void viewFeedback(Assignment assignment) {
+        if (!this.assignments.contains(assignment)) {
+            System.out.println("Assignment does not exist");
+        } else if (!assignment.getSubmissions().containsKey(this)) {
+            System.out.println("Assignment not submitted yet!");
+        } else {
+            Submission submission = assignment.getSubmissions().get(this);
+            if (submission != null && submission.isGraded()) {
+                System.out.println("Your grade: " + submission.getScore() + "\nFeedback: " + submission.getFeedback());
+            } else {
+                System.out.println("No feedback available yet");
+            }
+        }
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Student student = (Student) o;
-        return Objects.equals(schedule, student.schedule) && Objects.equals(assignments, student.assignments);
+        return studentId == student.studentId;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), schedule, assignments);
+        return Objects.hash(super.hashCode(), studentId);
     }
 
     @Override
     public String toString() {
         return "Student{" +
-                "schedule=" + schedule +
-                ", assignments=" + assignments +
+                "studentId=" + studentId +
+                ", courses=" + courses.stream()
+                .map(course -> "(Id: " + course.getCourseId() + ", Title: " + course.getTitle() + ")")
+                .collect(Collectors.joining(", ")) +
+                ", assignments=" + assignments.stream()
+                .map(AcademicItem::getTitle)
+                .collect(Collectors.joining(", ")) +
                 '}' + super.toString();
     }
 
-    public List<Course> getSchedule() {
-        return schedule;
+    public long getStudentId() {
+        return studentId;
     }
 
-    public void setSchedule(List<Course> schedule) {
-        this.schedule = schedule;
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     public List<Assignment> getAssignments() {
@@ -76,5 +150,13 @@ public class Student extends User {
 
     public void setAssignments(List<Assignment> assignments) {
         this.assignments = assignments;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Student.nextId = nextId;
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,25 +1,37 @@
 import org.example.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class StudentTest {
+    private final PrintStream originalOutPut = System.out;
+    private final ByteArrayOutputStream newOutPut = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(newOutPut));
+        newOutPut.reset();
+    }
+
+    @AfterEach
+    public void reset() {
+        System.setOut(originalOutPut);
+    }
+
 
     @Test
-    public void testDownloadSchedule() {
+    public void downloadScheduleTest() {
         Student student = new Student("bob", 22, User.Gender.MALE);
         Map<LocalDate, LocalTime> map = new TreeMap<>();
         map.put(LocalDate.now(), LocalTime.now());
         Teacher teacher = new Teacher("john", 56, User.Gender.MALE);
-        student.getSchedule().add(new Course("english", "xxxx", teacher, new ArrayList<>(), map));
+        student.getCourses().add(new Course("english", "xxxx", teacher, map, 7));
 
         student.downloadSchedule();
 
@@ -33,7 +45,133 @@ public class StudentTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         file.delete();
+    }
+
+    @Test
+    public void enrollCourseTest1() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Student student = new Student("james", 12, User.Gender.MALE);
+
+        student.enrollCourse(course);
+        Assertions.assertTrue(course.getStudents().contains(student));
+        Assertions.assertTrue(student.getCourses().contains(course));
+
+        student.enrollCourse(course);
+        Assertions.assertTrue(newOutPut.toString().contains("Student is already registered to the course"));
+        Assertions.assertFalse(student.getCourses().stream().count() == 2);
+    }
+
+    @Test
+    public void enrollCourseTest2() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+
+        for (int i = 0; i < 8; i++) {
+            student.enrollCourse(new Course("english", "xxx", teacher, new TreeMap<>(), 10));
+        }
+
+        Course course = new Course("french", "XXX", teacher, new TreeMap<>(), 9);
+        student.enrollCourse(course);
+
+        Assertions.assertTrue(newOutPut.toString().contains("maximum capacity of courses has been reached"));
+        Assertions.assertFalse(student.getCourses().contains(course));
+        Assertions.assertFalse(course.getStudents().contains(student));
+    }
+
+    @Test
+    public void submitAssignmentTest1() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+        student.getAssignments().add(assignment);
+
+        student.submitAssignment(assignment, new File(""));
+
+        Assertions.assertTrue(newOutPut.toString().contains("Assignment submitted successfully"));
+        Assertions.assertTrue(assignment.getSubmissions().containsKey(student));
+    }
+
+    @Test
+    public void submitAssignmentTest2() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+        student.submitAssignment(assignment, new File(""));
+
+        Assertions.assertTrue(newOutPut.toString().contains("Assignment does not exist"));
+        Assertions.assertFalse(assignment.getSubmissions().containsKey(student));
+    }
+
+    @Test
+    public void submitAssignmentTest3() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MIN, 10);
+        student.getAssignments().add(assignment);
+        student.submitAssignment(assignment, new File(""));
+
+        Assertions.assertTrue(newOutPut.toString().contains("Submission denied - past deadline!"));
+        Assertions.assertFalse(assignment.getSubmissions().containsKey(student));
+    }
+
+    @Test
+    public void viewFeedbackTest1() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+        student.getAssignments().add(assignment);
+        student.submitAssignment(assignment, new File(""));
+
+        assignment.getSubmissions().get(student).setGraded(true);
+        assignment.getSubmissions().get(student).setScore(10);
+        assignment.getSubmissions().get(student).setFeedback("xxx");
+
+        Submission submission = assignment.getSubmissions().get(student);
+        student.viewFeedback(assignment);
+        Assertions.assertTrue(newOutPut.toString().contains(String.format("Your grade: %s\nFeedback: %s", submission.getScore(), submission.getFeedback())));
+    }
+
+    @Test
+    public void viewFeedbackTest2() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+        student.getAssignments().add(assignment);
+        student.submitAssignment(assignment, new File(""));
+
+        student.viewFeedback(assignment);
+        Assertions.assertTrue(newOutPut.toString().contains("No feedback available yet"));
+    }
+
+    @Test
+    public void viewFeedbackTest3() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+
+        student.submitAssignment(assignment, new File(""));
+
+        student.viewFeedback(assignment);
+        Assertions.assertTrue(newOutPut.toString().contains("Assignment does not exist"));
+    }
+
+    @Test
+    public void viewFeedbackTest4() {
+        Teacher teacher = new Teacher("john", 34, User.Gender.MALE);
+        Student student = new Student("james", 12, User.Gender.MALE);
+        Course course = new Course("english", "xxx", teacher, new TreeMap<>(), 10);
+        Assignment assignment = new Assignment("xxx", "xxx", course, LocalDateTime.MAX, 10);
+        student.getAssignments().add(assignment);
+
+        student.viewFeedback(assignment);
+        Assertions.assertTrue(newOutPut.toString().contains("Assignment not submitted yet!"));
     }
 }
